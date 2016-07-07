e717e2ce2f1559ce016e2f6fd4763e5fc21b1f08
Shugmi Shumunov <SShumunov@gmail.com>
2016-07-07 17:52:19 -0400

    Improved functionality. Debugged some errors

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
 /* Play with FizzBuzz in the console */
-for (var i=0; i < 100; i++) {
-  if (i%3 === 0) {
-    console.log("Fizz" + '<br/>' );
-  } else if (i%5 === 0) {
+for (var i=0; i <= 100; i++) {
+  if (i%3 === 0 && i%5 === 0) {
+    console.log("Fizz" + '<br/>');
+  } else if (i%3 === 0) {
     console.log("Buzz" + '<br/>');
-  } else if (i%3 && i%5 === 0) {
+  } else if(i%5 === 0) {
         console.log("FizzBuzz" + '<br/>');
       } else {
         console.log(i + '<br/>');
 }
 }
-/* Print out FizzBuzz to the browser */ 
-for (var i=1; i < 100; i++) {
-  if (i%3 && i%5 === 0) {
-    document.write("FizzBuzz" + '<br/>' );
+/* Print out FizzBuzz to the browser */
+for (var i=1; i <= 100; i++) {
+  if (i%3 === 0 && i%5 === 0) {
+    document.write("FizzBuzz" + '<br/>');
   } else if (i%3 === 0) {
     document.write("Fizz" + '<br/>');
   } else if (i%5 === 0)  {
